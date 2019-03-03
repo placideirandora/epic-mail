f49e7f40e18b7801f934e8c7413652d0ec5789d8
Placide IRANDORA <placideirandora@gmail.com>
2019-03-03 15:33:56 +0200

    [chore #164320823] improve js code maintanability with eslint

--- UI/js/chat-bot.js
+++ UI/js/chat-bot.js
@@ -1,76 +1,79 @@
-
- let messages = [];
- let lastUserMessage = "";
- let botMessage = "";
- let botName = 'PLACIDE';
- let talking = true; 
- let sendButton = document.querySelector('#send-btn');
+const messages = [];
+let lastUserMessage = "";
+let botMessage = "";
+const botName = "PLACIDE";
+let talking = true;
+const sendButton = document.querySelector("#send-btn");
 
 function chatbotResponse() {
-    talking = true;
+  talking = true;
 
-    switch(lastUserMessage)
-    {
-        case 'hi':
-        botMessage = 'hi!';
-        break;
-
-        case 'hello':
-        botMessage = 'hello! ';
-        break;
-
-        case 'what is your name?':
-        botMessage = botName;
-        break;
-
-        case 'where do you live?':
-        botMessage = 'i live at kacyiru! ';
-        break;
-
-        case 'do you know andela?':
-        botMessage = 'of course! i like it.';
-        break;
-
-        default:
-        const random = ["i don't know", "feel better", "enjoy",
-         "interesting", "cool", "funny", "don't worry", "collaborate"]
-        botMessage = random[Math.floor(Math.random()*(random.length))];;
-        break;
-    }
-  
+  switch (lastUserMessage) {
+    case "hi":
+      botMessage = "hi!";
+      break;
+    case "hello":
+      botMessage = "hello! ";
+      break;
+    case "what is your name?":
+      botMessage = botName;
+      break;
+    case "where do you live?":
+      botMessage = "i live at kacyiru! ";
+      break;
+    case "do you know andela?":
+      botMessage = "of course! i like it.";
+      break;
+    default:
+      const random = [
+        "i don't know",
+        "feel better",
+        "enjoy",
+        "interesting",
+        "cool",
+        "funny",
+        "don't worry",
+        "collaborate",
+      ];
+      botMessage = random[Math.floor(Math.random() * random.length)];
+      break;
   }
-  
-  function newEntry() {
-    
-    if (document.querySelector("#chatbox").value != "")
-     {
-        lastUserMessage = document.querySelector("#chatbox").value;
-        document.querySelector("#chatbox").value = "";
-        messages.push("<b class='left'><i class='fa fa-user-circle'></i> INNOCENT:&nbsp;</b> " + lastUserMessage); 
-        chatbotResponse();
-        messages.push("<b class='right'><i class='fa fa-user-circle'></i> " + botName + ":</b> " + botMessage);
-        Speech(botMessage);
-        for (var i = 1; i < 15; i++) {
-            if (messages[messages.length - i])
-            document.querySelector("#chatlog" + i).innerHTML = messages[messages.length - i];
+}
+
+function newEntry() {
+  if (document.querySelector("#chatbox").value !== "") {
+    lastUserMessage = document.querySelector("#chatbox").value;
+    document.querySelector("#chatbox").value = "";
+    messages.push(
+      "<b class='left'><i class='fa fa-user-circle'></i> INNOCENT:&nbsp;</b> " + lastUserMessage,
+    );
+    chatbotResponse();
+    messages.push(
+      "<b class='right'><i class='fa fa-user-circle'></i> " + botName + ":</b> " + botMessage,
+    );
+    Speech(botMessage);
+    for (let i = 1; i < 15; i++) {
+      if (messages[messages.length - i]) {
+        document.querySelector("#chatlog" + i).innerHTML = messages[messages.length - i];
       }
     }
   }
-  
-  function Speech(say) {
-    if ('speechSynthesis' in window && talking) {
-      var utterance = new SpeechSynthesisUtterance(say);
-      speechSynthesis.speak(utterance);
-    }
-  }
-  
-  document.onkeypress = keyPress;
-  function keyPress(e) {
-    var x = e || window.event;
-    var key = (x.keyCode || x.which);
-    if (key == 13 || key == 3) {
-      newEntry();
-    }
-  }
+}
 
-  sendButton.addEventListener("click", newEntry);
+function Speech(say) {
+  if ("speechSynthesis" in window && talking) {
+    const utterance = new SpeechSynthesisUtterance(say);
+    speechSynthesis.speak(utterance);
+  }
+}
+
+document.onkeypress = keyPress;
+function keyPress(e) {
+  const x = e || window.event;
+  const key = x.keyCode || x.which;
+  if (key === 13 || key === 3) {
+    newEntry();
+  }
+}
+
+sendButton.addEventListener("click", newEntry);
--- UI/js/script.js
+++ UI/js/script.js
@@ -1,4 +1,4 @@
-//variables
+// variables
 
 const modal = document.querySelector(".modal");
 const resetLink = document.querySelector("#trigger");
@@ -7,52 +7,40 @@ const signupButton = document.querySelector("#sign-up-button");
 const signinButton = document.querySelector("#sign-in-button");
 const userForms = document.querySelector("#user-options-forms");
 
+// listeners
 
-//listeners
- 
 resetLink.addEventListener("click", togglemodal);
 resetButton.addEventListener("click", notification1);
 window.addEventListener("click", windowOnClick);
-signupButton.addEventListener("click", bounceLeft, false); 
+signupButton.addEventListener("click", bounceLeft, false);
 signinButton.addEventListener("click", bounceRight, false);
-newgroupButton.addEventListener("click", notification2);
 
+// helpers
 
-//helpers
-
-function togglemodal() 
-{
-    modal.classList.toggle("show-modal");
+function togglemodal() {
+  modal.classList.toggle("show-modal");
 }
 
-function notification1()
-{
-    alert("Password reset link has been sent to the email");
+function notification1() {
+  alert("Password reset link has been sent to the email");
 
-    if (alert)
-    {
-        togglemodal();
-    }
+  if (alert) {
+    togglemodal();
+  }
 }
 
-function windowOnClick(event) 
-{
-    if (event.target === modal) 
-    {
-        togglemodal();
-    }
+function windowOnClick(event) {
+  if (event.target === modal) {
+    togglemodal();
+  }
 }
 
-function bounceLeft()
-{
-        userForms.classList.remove("bounceRight");
-        userForms.classList.add("bounceLeft");
-    
+function bounceLeft() {
+  userForms.classList.remove("bounceRight");
+  userForms.classList.add("bounceLeft");
 }
 
-function bounceRight()
-{
-        userForms.classList.remove("bounceLeft");
-        userForms.classList.add("bounceRight");
-    
+function bounceRight() {
+  userForms.classList.remove("bounceLeft");
+  userForms.classList.add("bounceRight");
 }
