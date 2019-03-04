f082439c7d7e89aa3677f09aec8dea2855a293b1
Placide IRANDORA <placideirandora@gmail.com>
2019-03-04 17:26:26 +0200

    [feature #164351796] create models for user schema object and dummy data

---
+++ server/models/user.js
@@ -0,0 +1,11 @@
+class user {
+  constructor(id, firstname, lastname, email, password) {
+    this.id = id;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = email;
+    this.password = password;
+  }
+}
+
+export default user;
