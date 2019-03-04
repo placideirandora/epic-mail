976aea07227d591c97cef2d6df838eb4b8164c21
Placide IRANDORA <placideirandora@gmail.com>
2019-03-04 21:34:39 +0200

    [feature #164351810] add user login route

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,7 @@ import controller from "../controllers/user";
 
 const router = express.Router();
 
-router.post('/auth/signup', controller.registerUser);
+router.post("/auth/signup", controller.registerUser);
+router.post("/auth/login", controller.loginUser);
 
 export default router;
