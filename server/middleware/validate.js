b7aa2c9cac2bdbce59b6e2c6e1de5f7968601e76
Placide IRANDORA <placideirandora@gmail.com>
2019-03-05 02:12:31 +0200

    [feature #164351874] validate an email to be sent

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -22,6 +22,18 @@ const userParams = Joi.object().keys({
   userId: Joi.number().integer().required(),
 });
 
+const messageSchema = Joi.object().keys({
+  subject: Joi.string().alphanum().min(5).max(25)
+    .required(),
+  message: Joi.string().trim().min(10)
+    .max(100)
+    .required(),
+  parentMessageId: Joi.number().integer()
+    .required(),
+  status: Joi.string().alphanum().valid("sent", "draft", "read")
+    .required(),
+});
+
 export default {
-  userSchema, loginSchema, userParams,
+  userSchema, loginSchema, userParams, messageSchema,
 };
