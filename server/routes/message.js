9bac40652e1d9c7a93a36bbb286089fcf6dd49ce
Placide IRANDORA <placideirandora@gmail.com>
2019-03-05 20:14:20 +0200

    [feature #164351827] add a route for retrieving read emails

--- server/routes/message.js
+++ server/routes/message.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post("/messages", controller.sendEmail);
 router.get("/messages", controller.retrieveMails);
 router.get("/messages/sent", controller.retrieveSentEmails);
+router.get("/messages/read", controller.retrieveReadEmails);
 router.get("/messages/:id", controller.retrieveSpecificEmail);
 router.delete("/messages/:id", controller.deleteMail);
 
