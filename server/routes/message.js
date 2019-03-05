d127cef759acab25bee00602ed57644acf554f3b
Placide IRANDORA <placideirandora@gmail.com>
2019-03-05 03:26:18 +0200

    [feature #164351863] add a route for retrieving a specific email

--- server/routes/message.js
+++ server/routes/message.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post("/messages", controller.sendEmail);
 router.get("/messages", controller.retrieveMails);
+router.get("/messages/:id", controller.retrieveSpecificEmail);
 
 export default router;
