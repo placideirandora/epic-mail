6b30d33076207589c13a5704c03c76bd65cd46cc
Placide IRANDORA <placideirandora@gmail.com>
2019-03-06 19:37:15 +0200

    [chore #164413131] test more email endpoint functions

--- server/tests/message.js
+++ server/tests/message.js
@@ -1,7 +1,10 @@
 import chai from "chai";
 import chaiHTTP from "chai-http";
 import server from "../server";
-import { sentMessage, readMessage, draftMessage } from "./dummy";
+import {
+  sentMessage, readMessage, draftMessage, falseReadMessage, unregisteredReceiver,
+  unregisteredSender, emptyMessage,
+} from "./dummy";
 
 chai.use(chaiHTTP);
 chai.should();
@@ -52,6 +55,64 @@ describe("EMAIL ENDPOINT TESTS", () => {
         res.body.should.have.property("success").eql("email drafted");
         res.body.should.be.a("object");
         res.body.data.should.be.a("array");
+        done();
+      });
+  });
+
+  // eslint-disable-next-line no-undef
+  it("Should not send an email when the sender id and receiver id are the same", (done) => {
+    chai.request(server)
+      .post("/api/v1/messages")
+      .send(falseReadMessage)
+      .set("Accept", "Application/JSON")
+      .end((err, res) => {
+        res.body.should.have.status(400);
+        res.body.should.have.property("status").eql(400);
+        res.body.should.have.property("error").eql("the sender id and receiver id must not be the same");
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+
+  // eslint-disable-next-line no-undef
+  it("Should not send an email when the receiver is not registerd", (done) => {
+    chai.request(server)
+      .post("/api/v1/messages")
+      .send(unregisteredReceiver)
+      .set("Accept", "Application/JSON")
+      .end((err, res) => {
+        res.body.should.have.status(404);
+        res.body.should.have.property("status").eql(404);
+        res.body.should.have.property("error").eql("the receiver is not registered");
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+
+  // eslint-disable-next-line no-undef
+  it("Should not send an email when the sender is not registerd", (done) => {
+    chai.request(server)
+      .post("/api/v1/messages")
+      .send(unregisteredSender)
+      .set("Accept", "Application/JSON")
+      .end((err, res) => {
+        res.body.should.have.status(404);
+        res.body.should.have.property("status").eql(404);
+        res.body.should.have.property("error").eql("the sender is not registered");
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+
+  // eslint-disable-next-line no-undef
+  it("Should not send an email when the message is empty", (done) => {
+    chai.request(server)
+      .post("/api/v1/messages")
+      .send(emptyMessage)
+      .set("Accept", "Application/JSON")
+      .end((err, res) => {
+        res.body.should.have.property("error").eql("\"message\" is not allowed to be empty");
+        res.body.should.be.a("object");
         done();
       });
   });
@@ -163,6 +224,62 @@ describe("EMAIL ENDPOINT TESTS", () => {
         res.body.should.have.status(200);
         res.body.should.have.property("status").eql(200);
         res.body.should.have.property("success").eql("email deleted");
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+
+  // eslint-disable-next-line no-undef
+  it("Should delete a specific email 3", (done) => {
+    chai.request(server)
+      .delete("/api/v1/messages/3")
+      .set("Accept", "Application/JSON")
+      .end((err, res) => {
+        res.body.should.have.status(200);
+        res.body.should.have.property("status").eql(200);
+        res.body.should.have.property("success").eql("email deleted");
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+
+  // eslint-disable-next-line no-undef
+  it("Should not retrieve the email 1 because it will have been deleted", (done) => {
+    chai.request(server)
+      .get("/api/v1/messages/1")
+      .set("Accept", "Application/JSON")
+      .end((err, res) => {
+        res.body.should.have.status(404);
+        res.body.should.have.property("status").eql(404);
+        res.body.should.have.property("error").eql("email not found");
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+
+  // eslint-disable-next-line no-undef
+  it("Should not retrieve the email 2 because it will have been deleted", (done) => {
+    chai.request(server)
+      .get("/api/v1/messages/2")
+      .set("Accept", "Application/JSON")
+      .end((err, res) => {
+        res.body.should.have.status(404);
+        res.body.should.have.property("status").eql(404);
+        res.body.should.have.property("error").eql("email not found");
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+
+  // eslint-disable-next-line no-undef
+  it("Should not retrieve the email 3 because it will have been deleted", (done) => {
+    chai.request(server)
+      .get("/api/v1/messages/3")
+      .set("Accept", "Application/JSON")
+      .end((err, res) => {
+        res.body.should.have.status(404);
+        res.body.should.have.property("status").eql(404);
+        res.body.should.have.property("error").eql("email not found");
         res.body.should.be.a("object");
         done();
       });
