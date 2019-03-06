614e59e5959906d9efb8d766b5816d86d1a9c6e9
Placide IRANDORA <placideirandora@gmail.com>
2019-03-06 19:35:04 +0200

    [chore #164413131] add more dummy data for being used in tests

--- server/tests/dummy.js
+++ server/tests/dummy.js
@@ -66,7 +66,44 @@ const draftMessage = {
   status: "draft",
 };
 
+const falseReadMessage = {
+  subject: "Testing",
+  message: "Have you passed!",
+  senderId: 1,
+  receiverId: 1,
+  parentMessageId: 4,
+  status: "read",
+};
+
+const unregisteredReceiver = {
+  subject: "Testing",
+  message: "Have you passed!",
+  senderId: 1,
+  receiverId: 11,
+  parentMessageId: 4,
+  status: "draft",
+};
+
+const unregisteredSender = {
+  subject: "Testing",
+  message: "Have you passed!",
+  senderId: 20,
+  receiverId: 1,
+  parentMessageId: 4,
+  status: "draft",
+};
+
+const emptyMessage = {
+  subject: "Testing",
+  message: "",
+  senderId: 1,
+  receiverId: 3,
+  parentMessageId: 4,
+  status: "draft",
+};
+
 export {
   newUser, newUserLogIn, sentMessage, readMessage, draftMessage, falseEmailLogIn,
-  falsePasswdLogIn, falseNewUser, falseNewUser2, falseUserLogIn,
+  falsePasswdLogIn, falseNewUser, falseNewUser2, falseUserLogIn, falseReadMessage,
+  unregisteredReceiver, unregisteredSender, emptyMessage,
 };
