7859a6843220431f694abede4cbe71dfddaf55b3
Placide IRANDORA <placideirandora@gmail.com>
2019-03-08 02:02:08 +0200

    [bug #164482335] fix the false-positive response of the unregistered receiver and the same sender id and receiver id code blocks

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -18,43 +18,43 @@ const messages = {
       res.status(400).json({ error: error.details[0].message });
     } else {
       const trueSender = dummy.contacts.filter(sender => sender.id === senderId);
+      const trueReceiver = dummy.contacts.filter(receiver => receiver.id === receiverId);
+
       if (trueSender.length === 0) {
         res.status(404).json({
           status: 404,
           error: "the sender is not registered",
         });
-      }
-      const trueReceiver = dummy.contacts.filter(receiver => receiver.id === receiverId);
-      if (trueReceiver.length === 0) {
+      } else if (trueReceiver.length === 0) {
         res.status(404).json({
           status: 404,
           error: "the receiver is not registered",
         });
-      }
-      if (senderId === receiverId) {
+      } else if (senderId === receiverId) {
         res.status(400).json({
           status: 400,
           error: "the sender id and receiver id must not be the same",
         });
-      }
-      const id = dummy.messages.length + 1;
-      const createdOn = moment().format("LL");
-      const messagee = new Message(
-        id, createdOn, subject, message, senderId, receiverId, parentMessageId, status,
-      );
-      dummy.messages.push(messagee);
-      if (status === "sent") {
-        res.status(201).json({
-          status: 201, success: "email sent", data: [messagee],
-        });
-      } else if (status === "draft") {
-        res.status(201).json({
-          status: 201, success: "email drafted", data: [messagee],
-        });
       } else {
-        res.status(201).json({
-          status: 201, success: "email read", data: [messagee],
-        });
+        const id = dummy.messages.length + 1;
+        const createdOn = moment().format("LL");
+        const messagee = new Message(
+          id, createdOn, subject, message, senderId, receiverId, parentMessageId, status,
+        );
+        dummy.messages.push(messagee);
+        if (status === "sent") {
+          res.status(201).json({
+            status: 201, success: "email sent", data: [messagee],
+          });
+        } else if (status === "draft") {
+          res.status(201).json({
+            status: 201, success: "email drafted", data: [messagee],
+          });
+        } else {
+          res.status(201).json({
+            status: 201, success: "email read", data: [messagee],
+          });
+        }
       }
     }
   },
