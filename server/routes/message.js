ed7b63552265825a17e9598ca75e3952c526b092
Placide IRANDORA <placideirandora@gmail.com>
2019-03-14 23:11:12 +0200

    [chore #164646619] add a route for retrieving unread messages

--- server/routes/message.js
+++ server/routes/message.js
@@ -7,6 +7,7 @@ router.post("/messages", controller.sendEmail);
 router.get("/messages", controller.retrieveMails);
 router.get("/messages/sent", controller.retrieveSentEmails);
 router.get("/messages/read", controller.retrieveReadEmails);
+router.get("/messages/unread", controller.retrieveUnReadEmails);
 router.get("/messages/:id", controller.retrieveSpecificEmail);
 router.delete("/messages/:id", controller.deleteMail);
 
