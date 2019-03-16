a6ab88b47c2838634de9fedcb11af578401f691c
Placide IRANDORA <placideirandora@gmail.com>
2019-03-16 14:02:59 +0200

    [chore #164661037] add the middleware for validation

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
 import controller from "../controllers/user";
+import user from "../middleware/validate";
 
 const router = express.Router();
 
-router.post("/auth/signup", controller.registerUser);
-router.post("/auth/login", controller.loginUser);
+router.post("/auth/signup", user.validateRegistration, controller.registerUser);
+router.post("/auth/login", user.validateLogin, controller.loginUser);
 
 export default router;
