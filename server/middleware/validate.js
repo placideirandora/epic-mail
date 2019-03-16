87456103ca0f59a5414b1c3645a12c561ddf0131
Placide IRANDORA <placideirandora@gmail.com>
2019-03-16 14:04:13 +0200

    [chore #164661037] refactor the validation for acting as middleware

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -1,44 +1,82 @@
 import Joi from "joi";
 
-const userSchema = Joi.object().keys({
-  firstname: Joi.string().alphanum().min(5).max(15)
-    .required(),
-  lastname: Joi.string().alphanum().min(5).max(15)
-    .required(),
-  email: Joi.string().email({ minDomainAtomas: 2 })
-    .required(),
-  password: Joi.string().alphanum().min(3).max(15)
-    .required(),
-});
+const validate = {
 
-const loginSchema = Joi.object().keys({
-  email: Joi.string().email({ minDomainAtomas: 2 })
-    .required(),
-  password: Joi.string().alphanum().min(3).max(15)
-    .required(),
-});
+  validateRegistration(req, res, next) {
+    const userSchema = Joi.object().keys({
+      firstname: Joi.string().alphanum().min(5).max(15)
+        .required(),
+      lastname: Joi.string().alphanum().min(5).max(15)
+        .required(),
+      email: Joi.string().email({ minDomainAtomas: 2 })
+        .required(),
+      password: Joi.string().alphanum().min(3).max(15)
+        .required(),
+    });
 
-const messageSchema = Joi.object().keys({
-  subject: Joi.string().trim().min(5).max(25)
-    .required(),
-  message: Joi.string().trim().min(10)
-    .max(100)
-    .required(),
-  senderId: Joi.number().integer()
-    .required(),
-  receiverId: Joi.number().integer()
-    .required(),
-  parentMessageId: Joi.number().integer()
-    .required(),
-  status: Joi.string().alphanum().valid("sent", "draft", "read", "unread")
-    .required(),
-});
+    const { error } = Joi.validate(req.body, userSchema);
+    if (error) {
+      res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, "") });
+      return;
+    }
+    next();
+  },
 
-const emailParams = Joi.object().keys({
-  emailId: Joi.number().integer()
-    .required(),
-});
+  validateLogin(req, res, next) {
+    const loginSchema = Joi.object().keys({
+      email: Joi.string().email({ minDomainAtomas: 2 })
+        .required(),
+      password: Joi.string().alphanum().min(3).max(15)
+        .required(),
+    });
 
-export default {
-  userSchema, loginSchema, messageSchema, emailParams,
+    const { error } = Joi.validate(req.body, loginSchema);
+    if (error) {
+      res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, "") });
+      return;
+    }
+    next();
+  },
+
+  validateEmail(req, res, next) {
+    const messageSchema = Joi.object().keys({
+      subject: Joi.string().trim().min(5).max(25)
+        .required(),
+      message: Joi.string().trim().min(10)
+        .max(100)
+        .required(),
+      senderId: Joi.number().integer()
+        .required(),
+      receiverId: Joi.number().integer()
+        .required(),
+      parentMessageId: Joi.number().integer()
+        .required(),
+      status: Joi.string().alphanum().valid("sent", "draft", "read", "unread")
+        .required(),
+    });
+
+    const { error } = Joi.validate(req.body, messageSchema);
+    if (error) {
+      res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, "") });
+      return;
+    }
+    next();
+  },
+
+  validateEmailId(req, res, next) {
+    const emailParams = Joi.object().keys({
+      emailId: Joi.number().integer()
+        .required(),
+    });
+    const emailId = req.params.id;
+    const { error } = Joi.validate({ emailId }, emailParams);
+    if (error) {
+      res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, "") });
+      return;
+    }
+    next();
+  },
+
 };
+
+export default validate;
