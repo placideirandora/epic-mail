ea8a95a47bd95173d4b1aae85f496d3ea429563d
Placide IRANDORA <placideirandora@gmail.com>
2019-03-17 14:38:24 +0200

    [feature #164486874] add a route for deleting a group member

--- server/routes/group.js
+++ server/routes/group.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/groups", authenticate.verifyUser, group.validateGroup, controller.createGroup);
 router.delete("/groups/:id", authenticate.verifyUser, group.validateGroupId, controller.deleteGroup);
 router.post("/groups/:id/users", authenticate.verifyUser, group.validateGroupId, controller.addGroupMember);
+router.delete("/groups/:id/:users/:mid", authenticate.verifyUser, group.validateGroupIdAndMemberId, controller.deleteGroupMember);
 
 export default router;
