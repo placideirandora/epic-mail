5f3e29e8815716ee7b38f0d1f584e5be4635cdd5
Placide IRANDORA <placideirandora@gmail.com>
2019-03-17 21:32:17 +0200

    [feature #164683729] add a controller for retrieving group messages

--- server/controllers/group.js
+++ server/controllers/group.js
@@ -478,6 +478,66 @@ const groups = {
       res.status(500).json({ error: "error occured", error });
     });
   },
+
+  retrieveGroupEmails(req, res) {
+    const groupId = req.params.id;
+    const user = req.userId;
+    const userAccess = "true";
+    const findAdmin = database(sql.retrieveAdmin, [user, userAccess]);
+    findAdmin.then((response) => {
+      if (response.length !== 0) {
+        const specificGroup = database(sql.retrieveSpecificGroup, [groupId]);
+        specificGroup.then((response) => {
+          if (response.length === 0 || response.length === "undefined") {
+            res.status(404).json({ status: 404, error: "admin, group not found" });
+          } else {
+            const groupEmails = database(sql.retrieveSpecificGroupEmails, [groupId]);
+            groupEmails.then((response) => {
+              if (response.length === 0 || response.length === "undefined") {
+                res.status(404).json({ status: 404, error: "admin, no group emails found" });
+              } else {
+                res.status(200).json({
+                  status: 200,
+                  success: "admin, group emails found",
+                  data: response,
+                });
+              }
+            }).catch((error) => {
+              res.status(500).json({ error: "error occured", error });
+            });
+          }
+        }).catch((error) => {
+          res.status(500).json({ error: "error occured", error });
+        });
+      } else {
+        const userGroup = database(sql.retrieveUserGroup, [groupId, user]);
+        userGroup.then((response) => {
+          if (response.length === 0 || response.length === "undefined") {
+            res.status(404).json({ status: 404, error: "group not found" });
+          } else {
+            const groupEmails = database(sql.retrieveSpecificGroupEmails, [groupId]);
+            groupEmails.then((response) => {
+              if (response.length === 0 || response.length === "undefined") {
+                res.status(404).json({ status: 404, error: "no group emails found" });
+              } else {
+                res.status(200).json({
+                  status: 200,
+                  success: "group emails found",
+                  data: response,
+                });
+              }
+            }).catch((error) => {
+              res.status(500).json({ error: "error occured", error });
+            });
+          }
+        }).catch((error) => {
+          res.status(500).json({ error: "error occured", error });
+        });
+      }
+    }).catch((error) => {
+      res.status(500).json({ error: "error occured", error });
+    });
+  },
 };
 
 export default groups;
