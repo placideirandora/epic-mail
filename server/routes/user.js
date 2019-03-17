cd7db609cf1dcc7f4e4e8fa80138fb836d97c3b1
Placide IRANDORA <placideirandora@gmail.com>
2019-03-17 03:49:27 +0200

    [feature #164681243] add a route for retrieving a specific user

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/auth/signup", user.validateRegistration, controller.registerUser);
 router.post("/auth/login", user.validateLogin, controller.loginUser);
 router.get("/users", authenticate.verifyAdmin, controller.retrieveUsers);
+router.get("/users/:id", authenticate.verifyAdmin, user.validateUserId, controller.retrieveUser);
 
 export default router;
