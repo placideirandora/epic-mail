6cc2765b8e35f05d6b1c161924c1250393d956cb
Placide IRANDORA <placideirandora@gmail.com>
2019-03-17 15:26:11 +0200

    [feature #164487176] add a route for changing the group name

--- server/routes/group.js
+++ server/routes/group.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.post("/groups", authenticate.verifyUser, group.validateGroup, controller.createGroup);
 router.get("/groups", authenticate.verifyUser, controller.retrieveGroups);
+router.patch("/groups/:id/name", authenticate.verifyUser, group.validateGroupId, group.validateGroupName, controller.changeGroupName);
 router.delete("/groups/:id", authenticate.verifyUser, group.validateGroupId, controller.deleteGroup);
 router.post("/groups/:id/users", authenticate.verifyUser, group.validateGroupId, controller.addGroupMember);
 router.delete("/groups/:id/:users/:mid", authenticate.verifyUser, group.validateGroupIdAndMemberId, controller.deleteGroupMember);
