7522a5e9b6b5843fc60c7a931ce66b384788ad78
Placide IRANDORA <placideirandora@gmail.com>
2019-03-17 14:20:37 +0200

    [feature #164486826] create a group member model

---
+++ server/models/member.js
@@ -0,0 +1,10 @@
+class member {
+  constructor(firstname, lastname, role, memberGroup) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.role = role;
+    this.memberGroup = memberGroup;
+  }
+}
+
+export default member;
