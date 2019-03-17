efbf230ee35d9d8a4c53ea3598657fe8af6a9e4a
Placide IRANDORA <placideirandora@gmail.com>
2019-03-17 15:03:08 +0200

    [feature #164486903] add a route for sending a group message

--- server/routes/group.js
+++ server/routes/group.js
@@ -9,5 +9,6 @@ router.post("/groups", authenticate.verifyUser, group.validateGroup, controller.
 router.delete("/groups/:id", authenticate.verifyUser, group.validateGroupId, controller.deleteGroup);
 router.post("/groups/:id/users", authenticate.verifyUser, group.validateGroupId, controller.addGroupMember);
 router.delete("/groups/:id/:users/:mid", authenticate.verifyUser, group.validateGroupIdAndMemberId, controller.deleteGroupMember);
+router.post("/groups/:id/messages", authenticate.verifyUser, group.validateGroupEmail, controller.sendGroupEmail);
 
 export default router;
