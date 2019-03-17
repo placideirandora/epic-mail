7303b2d995c0b03725554d0de1ca108fb89970a6
Placide IRANDORA <placideirandora@gmail.com>
2019-03-17 20:41:30 +0200

    [feature #164683705] add a controller for retrieving group members

--- server/controllers/group.js
+++ server/controllers/group.js
@@ -309,6 +309,75 @@ const groups = {
     });
   },
 
+  retrieveGroupMembers(req, res) {
+    const groupId = req.params.id;
+    const user = req.userId;
+    const userAccess = "true";
+    const findAdmin = database(sql.retrieveAdmin, [user, userAccess]);
+    findAdmin.then((response) => {
+      if (response.length !== 0) {
+        const specificGroup = database(sql.retrieveSpecificGroup, [groupId]);
+        specificGroup.then((response) => {
+          if (response.length === 0 || response.length === "undefined") {
+            res.status(404).json({ status: 404, error: "admin, group not found" });
+          } else {
+            const groupMembers = database(sql.retrieveSpecificGroupMembers, [groupId]);
+            groupMembers.then((response) => {
+              if (response.length === 0 || response.length === "undefined") {
+                res.status(404).json({ status: 404, error: "admin, no group members found" });
+              } else {
+                res.status(404).json({
+                  status: 404,
+                  error: "admin, group members retrieved",
+                  data: response,
+                });
+              }
+            }).catch((error) => {
+              res.status(500).json({ error: "error occured", error });
+            });
+          }
+        }).catch((error) => {
+          res.status(500).json({ error: "error occured", error });
+        });
+      } else {
+        const userGroup = database(sql.retrieveUserGroup, [groupId, user]);
+        userGroup.then((response) => {
+          if (response.length === 0 || response.length === "undefined") {
+            res.status(404).json({ status: 404, error: "group not found" });
+          } else {
+            const specificGroup = database(sql.retrieveSpecificGroup, [groupId]);
+            specificGroup.then((response) => {
+              if (response.length === 0 || response.length === "undefined") {
+                res.status(404).json({ status: 404, error: "group not found" });
+              } else {
+                const groupMembers = database(sql.retrieveSpecificGroupMembers, [groupId]);
+                groupMembers.then((response) => {
+                  if (response.length === 0 || response.length === "undefined") {
+                    res.status(404).json({ status: 404, error: "no group members found" });
+                  } else {
+                    res.status(200).json({
+                      status: 200,
+                      success: "group members retrieved",
+                      data: response,
+                    });
+                  }
+                }).catch((error) => {
+                  res.status(500).json({ error: "error occured", error });
+                });
+              }
+            }).catch((error) => {
+              res.status(500).json({ error: "error occured", error });
+            });
+          }
+        }).catch((error) => {
+          res.status(500).json({ error: "error occured", error });
+        });
+      }
+    }).catch((error) => {
+      res.status(500).json({ error: "error occured", error });
+    });
+  },
+
   deleteGroupMember(req, res) {
     const groupId = req.params.id;
     const groupMemberId = req.params.mid;
