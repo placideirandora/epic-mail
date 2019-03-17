25ea899eac9e65e31d00427a3e966edadd858787
Placide IRANDORA <placideirandora@gmail.com>
2019-03-17 14:22:15 +0200

    [feature #164486826] add a route for registering a group member

--- server/routes/group.js
+++ server/routes/group.js
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post("/groups", authenticate.verifyUser, group.validateGroup, controller.createGroup);
 router.delete("/groups/:id", authenticate.verifyUser, group.validateGroupId, controller.deleteGroup);
+router.post("/groups/:id/users", authenticate.verifyUser, group.validateGroupId, controller.addGroupMember);
 
 export default router;
