b375c6410b8d2e1a68cdd3e845feeefd0d48fddc
Placide IRANDORA <placideirandora@gmail.com>
2019-03-17 02:07:19 +0200

    [chore #164680604] change the call back functions' names to correspond the controllers

--- server/routes/message.js
+++ server/routes/message.js
@@ -1,15 +1,17 @@
 import express from "express";
 import controller from "../controllers/message";
+import authenticate from "../middleware/authenticate";
 import email from "../middleware/validate";
 
 const router = express.Router();
 
-router.post("/messages", email.validateEmail, controller.sendEmail);
-router.get("/messages", controller.retrieveMails);
-router.get("/messages/sent", controller.retrieveSentEmails);
-router.get("/messages/read", controller.retrieveReadEmails);
-router.get("/messages/unread", controller.retrieveUnReadEmails);
-router.get("/messages/:id", email.validateEmailId, controller.retrieveSpecificEmail);
-router.delete("/messages/:id", email.validateEmailId, controller.deleteMail);
+router.post("/messages", authenticate.verifyUser, email.validateEmail, controller.sendEmail);
+router.get("/messages", authenticate.verifyUser, controller.retrieveMails);
+router.get("/messages/sent", authenticate.verifyUser, controller.retrieveSentEmails);
+router.get("/messages/read", authenticate.verifyUser, controller.retrieveReadEmails);
+router.get("/messages/unread", authenticate.verifyUser, controller.retrieveUnReadEmails);
+router.get("/messages/draft", authenticate.verifyUser, controller.retrieveDraftEmails);
+router.get("/messages/:id", authenticate.verifyUser, email.validateEmailId, controller.retrieveSpecificEmail);
+router.delete("/messages/:id", authenticate.verifyUser, email.validateEmailId, controller.deleteSpecificEmail);
 
 export default router;
