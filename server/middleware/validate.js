3439cb05a69c1214bc77c73cbf9d648ab744a945
Placide IRANDORA <placideirandora@gmail.com>
2019-03-17 02:03:51 +0200

    [chore #164680604] refactor, improve and add more validations

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -4,9 +4,9 @@ const validate = {
 
   validateRegistration(req, res, next) {
     const userSchema = Joi.object().keys({
-      firstname: Joi.string().alphanum().min(5).max(15)
+      firstname: Joi.string().alphanum().min(4).max(15)
         .required(),
-      lastname: Joi.string().alphanum().min(5).max(15)
+      lastname: Joi.string().alphanum().min(4).max(15)
         .required(),
       email: Joi.string().email({ minDomainAtomas: 2 })
         .required(),
@@ -40,12 +40,10 @@ const validate = {
 
   validateEmail(req, res, next) {
     const messageSchema = Joi.object().keys({
-      subject: Joi.string().trim().min(5).max(25)
+      subject: Joi.string().trim().min(2).max(25)
         .required(),
       message: Joi.string().trim().min(10)
-        .max(100)
-        .required(),
-      senderId: Joi.number().integer()
+        .max(800)
         .required(),
       receiverId: Joi.number().integer()
         .required(),
@@ -63,6 +61,34 @@ const validate = {
     next();
   },
 
+  validateUserId(req, res, next) {
+    const userParams = Joi.object().keys({
+      userId: Joi.number().integer()
+        .required(),
+    });
+    const userId = req.params.id;
+    const { error } = Joi.validate({ userId }, userParams);
+    if (error) {
+      res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, "") });
+      return;
+    }
+    next();
+  },
+
+  validateEmailAddr(req, res, next) {
+    const emailSchema = Joi.object().keys({
+      email: Joi.string().email({ minDomainAtomas: 2 })
+        .required(),
+    });
+
+    const { error } = Joi.validate(req.body, emailSchema);
+    if (error) {
+      res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, "") });
+      return;
+    }
+    next();
+  },
+
   validateEmailId(req, res, next) {
     const emailParams = Joi.object().keys({
       emailId: Joi.number().integer()
@@ -70,6 +96,105 @@ const validate = {
     });
     const emailId = req.params.id;
     const { error } = Joi.validate({ emailId }, emailParams);
+    if (error) {
+      res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, "") });
+      return;
+    }
+    next();
+  },
+
+  validateGroup(req, res, next) {
+    const groupSchema = Joi.object().keys({
+      name: Joi.string().trim().min(6).max(30)
+        .required(),
+      role: Joi.string().trim().min(5).max(60)
+        .required(),
+    });
+
+    const { error } = Joi.validate(req.body, groupSchema);
+    if (error) {
+      res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, "") });
+      return;
+    }
+    next();
+  },
+
+  validateGroupId(req, res, next) {
+    const groupIdParams = Joi.object().keys({
+      groupId: Joi.number().integer()
+        .required(),
+    });
+    const groupId = req.params.id;
+    const { error } = Joi.validate({ groupId }, groupIdParams);
+    if (error) {
+      res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, "") });
+      return;
+    }
+    next();
+  },
+
+  validateGroupName(req, res, next) {
+    const groupNameSchema = Joi.object().keys({
+      name: Joi.string().trim().min(6).max(30)
+        .required(),
+    });
+    const { error } = Joi.validate(req.body, groupNameSchema);
+    if (error) {
+      res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, "") });
+      return;
+    }
+    next();
+  },
+
+  validateGroupIdAndMemberId(req, res, next) {
+    const groupMemberParams = Joi.object().keys({
+      groupId: Joi.number().integer()
+        .required(),
+      memberId: Joi.number().integer()
+        .required(),
+    });
+    const groupId = req.params.id;
+    const memberId = req.params.mid;
+    const { error } = Joi.validate({ groupId, memberId }, groupMemberParams);
+    if (error) {
+      res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, "") });
+      return;
+    }
+    next();
+  },
+
+  validateGroupMember(req, res, next) {
+    const groupMemberSchema = Joi.object().keys({
+      firstname: Joi.string().alphanum().min(5).max(15)
+        .required(),
+      lastname: Joi.string().alphanum().min(5).max(15)
+        .required(),
+      role: Joi.string().trim().min(5).max(60)
+        .required(),
+    });
+
+    const { error } = Joi.validate(req.body, groupMemberSchema);
+    if (error) {
+      res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, "") });
+      return;
+    }
+    next();
+  },
+
+  validateGroupEmail(req, res, next) {
+    const messageSchema = Joi.object().keys({
+      subject: Joi.string().trim().min(2).max(25)
+        .required(),
+      message: Joi.string().trim().min(10)
+        .max(800)
+        .required(),
+      parentMessageId: Joi.number().integer()
+        .required(),
+      status: Joi.string().alphanum().valid("sent", "draft", "read", "unread")
+        .required(),
+    });
+
+    const { error } = Joi.validate(req.body, messageSchema);
     if (error) {
       res.status(400).json({ error: error.details[0].message.replace(/\\|(")/g, "") });
       return;
