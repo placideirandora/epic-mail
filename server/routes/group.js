886219a4e638ad8c0a90feac5e177546d1205329
Placide IRANDORA <placideirandora@gmail.com>
2019-03-17 15:18:46 +0200

    [feature #164487131] add a route for retrieving all groups

--- server/routes/group.js
+++ server/routes/group.js
@@ -6,6 +6,7 @@ import group from "../middleware/validate";
 const router = express.Router();
 
 router.post("/groups", authenticate.verifyUser, group.validateGroup, controller.createGroup);
+router.get("/groups", authenticate.verifyUser, controller.retrieveGroups);
 router.delete("/groups/:id", authenticate.verifyUser, group.validateGroupId, controller.deleteGroup);
 router.post("/groups/:id/users", authenticate.verifyUser, group.validateGroupId, controller.addGroupMember);
 router.delete("/groups/:id/:users/:mid", authenticate.verifyUser, group.validateGroupIdAndMemberId, controller.deleteGroupMember);
