a700ebd012a1b71519a2293ef200242a97cd1c85
Placide IRANDORA <placideirandora@gmail.com>
2019-03-20 13:01:42 +0200

    [chore #164689039] delete new users for the 'no users found' test to pass

--- server/tests/user.js
+++ server/tests/user.js
@@ -253,6 +253,58 @@ describe("USER ENDPOINT TESTS", () => {
       });
   });
 
+  it("Should delete a specific user of id = 4", (done) => {
+    chai.request(server)
+      .delete("/api/v2/users/4")
+      .set("authorization", adminToken)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("status").eql(200);
+        res.body.should.have.property("success").eql("user deleted");
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+
+  it("Should delete a specific user of id = 5", (done) => {
+    chai.request(server)
+      .delete("/api/v2/users/5")
+      .set("authorization", adminToken)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("status").eql(200);
+        res.body.should.have.property("success").eql("user deleted");
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+
+  it("Should delete a specific user of id = 6", (done) => {
+    chai.request(server)
+      .delete("/api/v2/users/6")
+      .set("authorization", adminToken)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("status").eql(200);
+        res.body.should.have.property("success").eql("user deleted");
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+
+  it("Should delete a specific user of id = 7", (done) => {
+    chai.request(server)
+      .delete("/api/v2/users/7")
+      .set("authorization", adminToken)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("status").eql(200);
+        res.body.should.have.property("success").eql("user deleted");
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+
   it("Should not delete the user of id = 3, because they will have been already deleted", (done) => {
     chai.request(server)
       .delete("/api/v2/users/3")
