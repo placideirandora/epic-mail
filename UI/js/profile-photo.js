d4fc1d3f9c04c9dcc307d2a48c22614b2d1037bd
Placide IRANDORA <placideirandora@gmail.com>
2019-03-26 00:06:04 +0200

    [chore #164852002] replace double quotes with single quotes for complying with eslint rules

--- UI/js/profile-photo.js
+++ UI/js/profile-photo.js
@@ -2,22 +2,22 @@
 
 function fileUpload() {
   function Init() {
-    const fileSelect = document.querySelector("#file-upload");
-    const fileDrag = document.querySelector("#file-drag");
+    const fileSelect = document.querySelector('#file-upload');
+    const fileDrag = document.querySelector('#file-drag');
 
-    fileSelect.addEventListener("change", fileSelectHandler, false);
-    fileDrag.addEventListener("dragover", fileDragHover, false);
-    fileDrag.addEventListener("dragleave", fileDragHover, false);
-    fileDrag.addEventListener("drop", fileSelectHandler, false);
+    fileSelect.addEventListener('change', fileSelectHandler, false);
+    fileDrag.addEventListener('dragover', fileDragHover, false);
+    fileDrag.addEventListener('dragleave', fileDragHover, false);
+    fileDrag.addEventListener('drop', fileSelectHandler, false);
   }
 
   function fileDragHover(e) {
-    const fileDrag = document.querySelector("#file-drag");
+    const fileDrag = document.querySelector('#file-drag');
 
     e.stopPropagation();
     e.preventDefault();
 
-    fileDrag.className = e.type === "dragover" ? "hover" : "modal-body file-upload";
+    fileDrag.className = e.type === 'dragover' ? 'hover' : 'modal-body file-upload';
   }
 
   function fileSelectHandler(e) {
@@ -31,36 +31,36 @@ function fileUpload() {
   }
 
   function output(msg) {
-    const m = document.querySelector("#messages");
+    const m = document.querySelector('#messages');
     m.innerHTML = msg;
   }
 
   function parseFile(file) {
-    output("<strong>" + encodeURI(file.name) + "</strong>");
+    output('<strong>' + encodeURI(file.name) + '</strong>');
 
     const imageName = file.name;
 
     const isGood = /\.(?=gif|jpg|png|jpeg)/gi.test(imageName);
 
     if (isGood) {
-      document.querySelector("#start").classList.add("hidden");
-      document.querySelector("#response").classList.remove("hidden");
-      document.querySelector("#notimage").classList.add("hidden");
-      document.querySelector("#file-image").classList.remove("hidden");
-      document.querySelector("#file-image").src = URL.createObjectURL(file);
+      document.querySelector('#start').classList.add('hidden');
+      document.querySelector('#response').classList.remove('hidden');
+      document.querySelector('#notimage').classList.add('hidden');
+      document.querySelector('#file-image').classList.remove('hidden');
+      document.querySelector('#file-image').src = URL.createObjectURL(file);
     } else {
-      document.querySelector("#file-image").classList.add("hidden");
-      document.querySelector("#notimage").classList.remove("hidden");
-      document.querySelector("#start").classList.remove("hidden");
-      document.querySelector("#response").classList.add("hidden");
-      document.querySelector("#file-upload-form").reset();
+      document.querySelector('#file-image').classList.add('hidden');
+      document.querySelector('#notimage').classList.remove('hidden');
+      document.querySelector('#start').classList.remove('hidden');
+      document.querySelector('#response').classList.add('hidden');
+      document.querySelector('#file-upload-form').reset();
     }
   }
 
   if (window.File && window.FileReader && window.FileList) {
     Init();
   } else {
-    alert("The File APIs are not fully supported in this browser.");
+    alert('The File APIs are not fully supported in this browser.');
   }
 }
 
