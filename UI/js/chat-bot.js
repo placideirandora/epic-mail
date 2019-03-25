a0db08f3bbd1daf2f78ff92936b0a6e182b72dc6
Placide IRANDORA <placideirandora@gmail.com>
2019-03-25 23:58:42 +0200

    [chore #164852002] replace double quotes with single quotes for eslint rules

--- UI/js/chat-bot.js
+++ UI/js/chat-bot.js
@@ -1,39 +1,40 @@
+/* eslint-disable no-case-declarations */
 const messages = [];
-let lastUserMessage = "";
-let botMessage = "";
-const botName = "PLACIDE";
+let lastUserMessage = '';
+let botMessage = '';
+const botName = 'PLACIDE';
 let talking = true;
-const sendButton = document.querySelector("#send-btn");
+const sendButton = document.querySelector('#send-btn');
 
 function chatbotResponse() {
   talking = true;
 
   switch (lastUserMessage) {
-    case "hi":
-      botMessage = "hi!";
+    case 'hi':
+      botMessage = 'hi!';
       break;
-    case "hello":
-      botMessage = "hello! ";
+    case 'hello':
+      botMessage = 'hello! ';
       break;
-    case "what is your name?":
+    case 'what is your name?':
       botMessage = botName;
       break;
-    case "where do you live?":
-      botMessage = "i live at kacyiru! ";
+    case 'where do you live?':
+      botMessage = 'i live at kacyiru! ';
       break;
-    case "do you know andela?":
-      botMessage = "of course! i like it.";
+    case 'do you know andela?':
+      botMessage = 'of course! i like it.';
       break;
     default:
       const random = [
-        "i don't know",
-        "feel better",
-        "enjoy",
-        "interesting",
-        "cool",
-        "funny",
-        "don't worry",
-        "collaborate",
+        'i do not know',
+        'feel better',
+        'enjoy',
+        'interesting',
+        'cool',
+        'funny',
+        'do not worry',
+        'collaborate',
       ];
       botMessage = random[Math.floor(Math.random() * random.length)];
       break;
@@ -41,9 +42,9 @@ function chatbotResponse() {
 }
 
 function newEntry() {
-  if (document.querySelector("#chatbox").value !== "") {
-    lastUserMessage = document.querySelector("#chatbox").value;
-    document.querySelector("#chatbox").value = "";
+  if (document.querySelector('#chatbox').value !== '') {
+    lastUserMessage = document.querySelector('#chatbox').value;
+    document.querySelector('#chatbox').value = '';
     messages.push(
       "<b class='left'><i class='fa fa-user-circle'></i> INNOCENT:&nbsp;</b> " + lastUserMessage,
     );
@@ -54,14 +55,14 @@ function newEntry() {
     Speech(botMessage);
     for (let i = 1; i < 15; i++) {
       if (messages[messages.length - i]) {
-        document.querySelector("#chatlog" + i).innerHTML = messages[messages.length - i];
+        document.querySelector('#chatlog' + i).innerHTML = messages[messages.length - i];
       }
     }
   }
 }
 
 function Speech(say) {
-  if ("speechSynthesis" in window && talking) {
+  if ('speechSynthesis' in window && talking) {
     const utterance = new SpeechSynthesisUtterance(say);
     speechSynthesis.speak(utterance);
   }
@@ -76,4 +77,4 @@ function keyPress(e) {
   }
 }
 
-sendButton.addEventListener("click", newEntry);
+sendButton.addEventListener('click', newEntry);
