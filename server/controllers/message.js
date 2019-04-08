7eac73c45992304a7a78149cc15f7243cc64bf11
Placide IRANDORA <placideirandora@gmail.com>
2019-04-08 12:00:29 +0200

    [chore #164852002] allow the admin to retrieve a specific user sent email

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -189,14 +189,14 @@ const messages = {
     const findAdmin = database(sql.retrieveAdmin, [user, userAccess]);
     findAdmin.then((response) => {
       if (response.length !== 0) {
-        const specificEmail = database(sql.retrieveSpecificSentEmail, [emailId, user]);
+        const specificEmail = database(sql.adminRetrieveUserSpecificSentEmail, [emailId]);
         specificEmail.then((response) => {
           if (response.length === 0 || response.length === 'undefined') {
-            res.status(404).json({ status: 404, error: 'sent email not found' });
+            res.status(404).json({ status: 404, error: 'admin, sent email not found' });
           } else {
             res.status(200).json({
               status: 200,
-              success: 'sent email retrieved',
+              success: 'admin, sent email retrieved',
               data: response,
             });
           }
