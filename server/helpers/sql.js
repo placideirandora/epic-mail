1fdb7e57303db6c4745d47f31fd311b1a6ff4992
Placide IRANDORA <34879554+placiderapson@users.noreply.github.com>
2021-08-01 23:23:07 +0200

    Update the Raw SQL Queries due to the Database Restructuring

--- server/helpers/sql.js
+++ server/helpers/sql.js
@@ -1,75 +1,71 @@
-/**
- * an sql object for storing all sql queries
- */
 const sql = {};
 
 /**
- * sql queries for processing data
+ * Declare SQL Queries for Data Processing
  */
-const admin = 'INSERT INTO users(firstname, lastname, username, email, password, isadmin, registered) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT (email) DO NOTHING';
-const registerUser = 'INSERT INTO users(firstname, lastname, username, email, password, registered) VALUES ($1, $2, $3, $4, $5, $6) RETURNING * ';
+const seedAdmin = 'INSERT INTO users(firstname, lastname, username, email, password, isActive, isAdmin) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT (email) DO NOTHING';
+const registerUser = 'INSERT INTO users(firstname, lastname, username, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING * ';
 const retrieveAllUsers = 'SELECT * FROM users';
 const loginUser = 'SELECT * FROM users WHERE email = $1';
 const retrieveSpecificUser = 'SELECT * FROM users WHERE email = $1';
 const retrieveSpecificUserById = 'SELECT * FROM users WHERE id = $1';
 const deleteSpecificUser = 'DELETE FROM users WHERE id = $1';
-const sendEmail = 'INSERT INTO sentemails(subject, message, parentmessageid, senderemail, receiveremail, status, createdon) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *';
-const draftEmail = 'INSERT INTO draftemails(subject, message, parentmessageid, senderemail, receiveremail, status, createdon) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *';
-const delivered = 'INSERT INTO receivedemails(subject, message, parentmessageid, senderemail, receiveremail, status, createdon) VALUES ($1, $2, $3, $4, $5, $6, $7)';
-const emailRead = 'UPDATE receivedemails SET status = $1 WHERE id = $2 RETURNING *';
-const retrieveAllEmails = 'SELECT * FROM receivedemails';
-const retrieveSpecificReceivedEmail = 'SELECT * FROM receivedemails WHERE id = $1 AND receiveremail = $2';
-const retrieveSpecificSentEmail = 'SELECT * FROM sentemails WHERE id = $1 AND senderemail = $2';
-const retrieveSpecificDraftEmail = 'SELECT * FROM draftemails WHERE id = $1 AND senderemail = $2';
-const deleteSpecificEmail = 'DELETE FROM receivedemails WHERE id = $1';
-const deleteSpecificSentEmail = 'DELETE FROM sentemails WHERE id = $1';
-const deleteSpecificDraftEmail = 'DELETE FROM draftemails WHERE id = $1';
-const retrieveSentEmails = 'SELECT * FROM sentemails WHERE senderemail = $1';
-const retrieveReadEmails = 'SELECT * FROM receivedemails WHERE status = $1 AND receiveremail = $2';
-const retrieveUnreadEmails = 'SELECT * FROM receivedemails WHERE status = $1 AND receiveremail = $2';
-const retrieveDraftEmails = 'SELECT * FROM draftemails WHERE status = $1 AND senderemail = $2';
+const sendEmail = 'INSERT INTO sentEmails(subject, message, senderEmail, receiverEmail, status) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+const draftEmail = 'INSERT INTO draftEmails(subject, message, senderEmail, receiverEmail, status) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+const delivered = 'INSERT INTO receivedEmails(subject, message, senderEmail, receiverEmail, status) VALUES ($1, $2, $3, $4, $5)';
+const emailRead = 'UPDATE receivedEmails SET status = $1 WHERE id = $2 RETURNING *';
+const retrieveAllEmails = 'SELECT * FROM receivedEmails';
+const retrieveSpecificReceivedEmail = 'SELECT * FROM receivedEmails WHERE id = $1 AND receiverEmail = $2';
+const retrieveSpecificSentEmail = 'SELECT * FROM sentEmails WHERE id = $1 AND senderEmail = $2';
+const retrieveSpecificDraftEmail = 'SELECT * FROM draftEmails WHERE id = $1 AND senderEmail = $2';
+const deleteSpecificEmail = 'DELETE FROM receivedEmails WHERE id = $1';
+const deleteSpecificSentEmail = 'DELETE FROM sentEmails WHERE id = $1';
+const deleteSpecificDraftEmail = 'DELETE FROM draftEmails WHERE id = $1';
+const retrieveSentEmails = 'SELECT * FROM sentEmails WHERE senderEmail = $1';
+const retrieveReadEmails = 'SELECT * FROM receivedEmails WHERE status = $1 AND receiverEmail = $2';
+const retrieveUnreadEmails = 'SELECT * FROM receivedEmails WHERE status = $1 AND receiverEmail = $2';
+const retrieveDraftEmails = 'SELECT * FROM draftEmails WHERE status = $1 AND senderEmail = $2';
 const findUserEmail = 'SELECT * FROM users WHERE email = $1';
 const findUsername = 'SELECT * FROM users WHERE username = $1';
-const retrieveAdmin = 'SELECT * FROM users WHERE email = $1 AND isadmin = $2';
-const retrieveUserSpecificReceivedEmail = 'SELECT * FROM receivedemails WHERE id = $1 AND receiveremail = $2';
-const adminRetrieveUserSpecificReceivedEmail = 'SELECT * FROM receivedemails WHERE id = $1';
-const retrieveUserSpecificSentEmail = 'SELECT * FROM sentemails WHERE id = $1 AND senderemail = $2';
-const adminRetrieveUserSpecificSentEmail = 'SELECT * FROM sentemails WHERE id = $1';
-const retrieveUserSpecificDraftEmail = 'SELECT * FROM draftemails WHERE id = $1 AND senderemail = $2';
-const adminRetrieveUserSpecificDraftEmail = 'SELECT * FROM draftemails WHERE id = $1';
-const adminGetSentEmails = 'SELECT * FROM sentemails';
-const adminGetReadEmails = 'SELECT * FROM receivedemails WHERE status = $1';
-const adminGetUnreadEmails = 'SELECT * FROM receivedemails WHERE status = $1';
-const adminGetDraftEmails = 'SELECT * FROM draftemails WHERE status = $1';
-const createGroup = 'INSERT INTO groups(name, role, owner) VALUES($1, $2, $3) RETURNING *';
+const retrieveAdmin = 'SELECT * FROM users WHERE email = $1 AND isAdmin = $2';
+const retrieveUserSpecificReceivedEmail = 'SELECT * FROM receivedEmails WHERE id = $1 AND receiverEmail = $2';
+const adminRetrieveUserSpecificReceivedEmail = 'SELECT * FROM receivedEmails WHERE id = $1';
+const retrieveUserSpecificSentEmail = 'SELECT * FROM sentEmails WHERE id = $1 AND senderEmail = $2';
+const adminRetrieveUserSpecificSentEmail = 'SELECT * FROM sentEmails WHERE id = $1';
+const retrieveUserSpecificDraftEmail = 'SELECT * FROM draftEmails WHERE id = $1 AND senderEmail = $2';
+const adminRetrieveUserSpecificDraftEmail = 'SELECT * FROM draftEmails WHERE id = $1';
+const adminGetSentEmails = 'SELECT * FROM sentEmails';
+const adminGetReadEmails = 'SELECT * FROM receivedEmails WHERE status = $1';
+const adminGetUnreadEmails = 'SELECT * FROM receivedEmails WHERE status = $1';
+const adminGetDraftEmails = 'SELECT * FROM draftEmails WHERE status = $1';
+const createGroup = 'INSERT INTO groups(name, purpose, owner) VALUES($1, $2, $3) RETURNING *';
 const retrieveAllGroups = 'SELECT * FROM groups';
 const retrieveSpecificGroup = 'SELECT * FROM groups WHERE id = $1';
 const updateSpecificGroup = 'UPDATE groups SET name = $1 WHERE id = $2 RETURNING *';
 const deleteSpecificGroup = 'DELETE FROM groups WHERE id = $1';
-const registerGroupMember = 'INSERT INTO groupmembers(username, email, groupid) VALUES ($1, $2, $3) RETURNING * ';
+const registerGroupMember = 'INSERT INTO groupMembers(username, email, groupId) VALUES ($1, $2, $3) RETURNING * ';
 const retrieveSpecificGroupOwner = 'SELECT * FROM groups WHERE id = $1 AND owner = $2';
-const retrieveSpecificGroupMember = 'SELECT * FROM groupmembers WHERE email = $1 AND groupid = $2';
-const retrieveSpecificGroupMemberById = 'SELECT * FROM groupmembers WHERE id = $1 AND groupid = $2';
-const deleteSpecificGroupMember = 'DELETE FROM groupmembers WHERE id = $1 AND groupid = $2';
-const sendGroupEmail = 'INSERT INTO groupmessages(subject, message, parentmessageid, createdon, groupid) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+const retrieveSpecificGroupMember = 'SELECT * FROM groupMembers WHERE email = $1 AND groupId = $2';
+const retrieveSpecificGroupMemberById = 'SELECT * FROM groupMembers WHERE id = $1 AND groupId = $2';
+const deleteSpecificGroupMember = 'DELETE FROM groupMembers WHERE id = $1 AND groupId = $2';
+const sendGroupEmail = 'INSERT INTO groupMessages(message, groupId) VALUES ($1, $2) RETURNING *';
 const deleteSpecificUserPassword = 'UPDATE users SET password = null WHERE email = $1 ';
 const retrieveUserGroups = 'SELECT * FROM groups WHERE owner = $1';
 const retrieveUserGroup = 'SELECT * FROM groups WHERE id = $1 AND owner = $2';
-const retrieveSpecificGroupMembers = 'SELECT * FROM groupmembers WHERE groupid = $1';
-const retrieveSpecificGroupEmails = 'SELECT * FROM groupmessages WHERE groupid = $1';
+const retrieveSpecificGroupMembers = 'SELECT * FROM groupMembers WHERE groupId = $1';
+const retrieveSpecificGroupEmails = 'SELECT * FROM groupMessages WHERE groupId = $1';
 const retrievePassResetUsers = 'SELECT * FROM users WHERE password isNull';
-const retrieveMemberByUsername = 'SELECT * FROM groupmembers WHERE username = $1 AND groupid = $2';
-const retrieveMemberByEmail = 'SELECT * FROM groupmembers WHERE email = $1 AND groupid = $2';
+const retrieveMemberByUsername = 'SELECT * FROM groupMembers WHERE username = $1 AND groupId = $2';
+const retrieveMemberByEmail = 'SELECT * FROM groupMembers WHERE email = $1 AND groupId = $2';
 const findGroup = 'SELECT * FROM groups WHERE name = $1 AND owner = $2';
-const retrieveSpecificUserEmails = 'SELECT * FROM receivedemails WHERE receiveremail = $1';
+const retrieveSpecificUserEmails = 'SELECT * FROM receivedEmails WHERE receiverEmail = $1';
 const passResetCheck = 'SELECT * FROM users WHERE email = $1 AND password isNull';
-const retrieveMemberEmails = 'SELECT * FROM groupmessages WHERE groupid = $1';
-
+const retrieveMemberEmails = 'SELECT * FROM groupMessages WHERE groupId = $1';
 
 /**
- * assigning object keys their corresponding values
+ * Add the Queries into the SQL Object
  */
-sql.admin = admin;
+sql.seedAdmin = seedAdmin;
 sql.registerUser = registerUser;
 sql.loginUser = loginUser;
 sql.retrieveAllUsers = retrieveAllUsers;
